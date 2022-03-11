46ce42d5482151d720b7d0e53302995aece84347
Thomas <thomasyayo15@gmail.com>
2022-03-11 01:28:09 +0100

    Importating enemies sprites, reworked a little the first map and added starting animation to it. Started to work on player, currently can move to right nor left and jump, player animation also adapt to the current movements. Shader script added to animate background.

--- src/entities/player/player.gd
+++ src/entities/player/player.gd
@@ -1,16 +1,39 @@
 extends KinematicBody2D
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+onready var animation : AnimationPlayer = $AnimationPlayer;
+onready var sprite : Sprite = $Sprite;
 
+export (int) var speed = 125;
+export (int) var jump_speed = -350;
+export (int) var gravity = 1000;
 
-# Called when the node enters the scene tree for the first time.
+var velocity = Vector2.ZERO;
+
 func _ready():
-	pass # Replace with function body.
+	animation.play("idle");
 
+func get_input():
+	velocity.x = 0;
+	if Input.is_action_pressed("walk_right"):
+		velocity.x += speed;
+		sprite.flip_h = false;
+	elif Input.is_action_pressed("walk_left"):
+		velocity.x -= speed;
+		sprite.flip_h = true;
+	if velocity.y != 0: animation.play("jump")
+	elif velocity.x != 0: animation.play("walk")
+	elif velocity == Vector2(0, 0):
+		pass
+#		animation.play("idle")
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _physics_process(delta):
+	get_input();
+	velocity.y += gravity * delta;
+	velocity = move_and_slide(velocity, Vector2.UP);
+	if Input.is_action_just_pressed("jump"):
+		if is_on_floor():
+			velocity.y = jump_speed;
+	elif velocity == Vector2(0, 0):
+		sprite.hframes = 11;
+		animation.play("idle")
---
+++ src/sandbox/sandbox_map.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+var camera_target : Node2D;
+
+func _ready():
+	_set_up_camera();
+	$Decorations/AnimationPlayer1.play("animation0");
+
+func _process(_delta: float) -> void:
+	$Camera2D.position = camera_target.position;
+
+func _set_up_camera():
+#	var rect : Rect2 = $Map.get_used_rect();
+#	var camera : Camera2D = $Camera2D;
+#	camera.set_limit(0, rect.position.x * 16 + 16);
+#	camera.set_limit(1, rect.position.y * 16);
+#	camera.set_limit(2, rect.end.x * 16 - 16);
+#	camera.set_limit(3, rect.end.y * 16);
+	camera_target = $Player;
